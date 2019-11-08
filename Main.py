31b4c690ef33d61a925b8946347d353b483a653d
jodorganistaca <jodorganistaca@unal.edu.co>
2019-11-07 23:00:39 -0500

    :construction: add methods and work in create a franchise

--- Main.py
+++ Main.py
@@ -18,19 +18,47 @@ class Vehiculo:
             self.motor.setPresion(presion)
             velocidad = self.motor.getVelocidad()
         return velocidad
+    
+    def  instalacionPiezas(self):
+        print('instalando piezas...')
+    
+    def pintura(self):
+        print('pintura....')
+        
+    def chasisCarroceria(self):
+        print('ensambe del chasis y la carroceria...')
+class Ford:
+
+    motor = None
+
+    def setMotor(self, motor):
+        self.motor = motor
+
+    def aceleracion(self,presion):
+        if not self.motor is None:
+            self.motor.setPresion(presion)
+            velocidad = self.motor.getVelocidad()
+        return velocidad
 
 class VehiculoFactory:
-    def construirVehiculo(self):
+    def construirVehiculo(self, tipoVehiculo):
         vehiculo = Vehiculo()
+        if(tipoVehiculo == 'Ford'):
+            print('Soy ford')
+        elif(tipoVehiculo == 'Hyundai'):
+            print('Soy Hyundai')
         motor = Motor()
         vehiculo.setMotor(motor)
+        vehiculo.instalacionPiezas()
+        vehiculo.pintura()
+        vehiculo.chasisCarroceria()
         return vehiculo;
 
 vF = VehiculoFactory()
 
-v1 = vF.construirVehiculo()
-v2 = vF.construirVehiculo()
-v3 = vF.construirVehiculo()
+v1 = vF.construirVehiculo('Ford')
+v2 = vF.construirVehiculo('Hyundai')
+v3 = vF.construirVehiculo('Ford')
 
 print(v1.aceleracion(5))
 print(v2.aceleracion(7))
