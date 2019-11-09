b09651e61f78cbb00c60821a03427a1b4dcecff7
marobayos <marobayos@unal.edu.co>
2019-11-09 18:38:34 -0500

    :seedling: Adding new classes

--- Main.py
+++ Main.py
@@ -6,32 +6,57 @@ class Motor:
     def getVelocidad(self):
         return self.presion*self.torque
 
-class Vehiculo:
 
+class Vehiculo:
     motor = None
+
+    def transporta(self):
+        """ este metodo se debe redefinir """
+        pass
 
     def setMotor(self, motor):
         self.motor = motor
 
-    def aceleracion(self,presion):
+    def aceleracion(self, presion):
         if not self.motor is None:
             self.motor.setPresion(presion)
             velocidad = self.motor.getVelocidad()
         return velocidad
 
+
+class Bus (Vehiculo):
+
+    def transporta(self):
+        return "Transporta personas"
+
+
+class Camion(Vehiculo):
+
+    def transporta(self):
+        return "Transporta carga o mercancias"
+
+
 class VehiculoFactory:
-    def construirVehiculo(self):
-        vehiculo = Vehiculo()
+    def construirVehiculo(self, tipoCarga):
+        if "Personas" == tipoCarga:
+            vehiculo = Bus()
+        elif "Carga" == tipoCarga:
+            vehiculo = Camion()
+        else:
+            vehiculo = Vehiculo()
         motor = Motor()
         vehiculo.setMotor(motor)
         return vehiculo;
 
 vF = VehiculoFactory()
 
-v1 = vF.construirVehiculo()
-v2 = vF.construirVehiculo()
-v3 = vF.construirVehiculo()
+v1 = vF.construirVehiculo("Personas")
+v2 = vF.construirVehiculo("Carga")
+v3 = vF.construirVehiculo("Otros")
 
 print(v1.aceleracion(5))
+print(v1.transporta())
 print(v2.aceleracion(7))
+print(v2.transporta())
 print(v3.aceleracion(10))
+print(v3.transporta())
