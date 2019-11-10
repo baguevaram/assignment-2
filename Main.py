a3afc451b25c9744e09cc67ac50322470858c1a4
marobayos <marobayos@unal.edu.co>
2019-11-09 21:35:43 -0500

    :sparkles: Decorador para la descripcion de los vehiculos

--- Main.py
+++ Main.py
@@ -9,22 +9,13 @@ class Motor:
 
 class Vehiculo:
     motor = None
-    marca = None
 
     def setMotor(self, motor):
         self.motor = motor
-        
-    def transporta(self):
+    
+    def marca(self, marca):
         """ este metodo se debe redefinir """
         pass
-    
-    def setMarca(self, marca):
-        self.marca = marca
-        """ este metodo se debe redefinir """
-        pass
-    
-    def getMarca(self):
-        return self.marca
 
     def aceleracion(self, presion):
         if not self.motor is None:
@@ -40,73 +31,87 @@ class Vehiculo:
         
     def chasisCarroceria(self):
         print('ensambe del chasis y la carroceria...')
-    
-class Bus (Vehiculo):
-    def transporta(self):
-        return "Transporta personas"
+
+    def descipcion(self):
+        return 'Soy un vehiculo'
+
+    def pintar(self, color):
+        anteriorDescripcion = self.descipcion
+        def nuevaDescripcion():
+            return anteriorDescripcion() + ", de color " + color
+        self.descipcion = nuevaDescripcion
+
+    def agregarPuertas(self, cantidad):
+        anteriorDescripcion = self.descipcion
+        def nuevaDescripcion():
+            return anteriorDescripcion() + ", con " + str(cantidad) + " puertas"
+        self.descipcion = nuevaDescripcion
+
+    def establecerCapacidad(self, capacidad):
+        anteriorDescripcion = self.descipcion
+        def nuevaDescripcion():
+            return anteriorDescripcion() + ", con capacidad para " + str(capacidad) + " personas"
+        self.descipcion = nuevaDescripcion
+
+    def elegirPlaca(self, placa):
+        anteriorDescripcion = self.descipcion
+        def nuevaDescripcion():
+            return anteriorDescripcion() + ", con placa " + placa
+        self.descipcion = nuevaDescripcion
 
 
-class Camion(Vehiculo):
-    def transporta(self):
-        return "Transporta carga o mercancias"
-    
 class Ford(Vehiculo):
     def marca(self):
         return "Ford"
 
+    def descipcion(self):
+        return Vehiculo.descipcion(self) + " de marca Ford"
+
+
 class Hyundai(Vehiculo):
     def marca(self):
         return "Hyundai"
-    
+
+    def descipcion(self):
+        return Vehiculo.descipcion(self) + " de marca Hyundai"
+
 class Mercedes(Vehiculo):
     def marca(self):
         return "Mercedes"
 
-
-class Bus (Vehiculo):
-    def transporta(self):
-        return "Transporta personas"
-
-
-class Camion(Vehiculo):
-    def transporta(self):
-        return "Transporta carga o mercancias"
-
+    def descipcion(self):
+        return Vehiculo.descipcion(self) + " de marca Mercedes"
 
 class VehiculoFactory:
-    def construirVehiculo(self, tipoCarga, tipoVehiculo):        
-        if "Personas" == tipoCarga:
-            vehiculo = Bus()
-        elif "Carga" == tipoCarga:
-            vehiculo = Camion()
-        else:
-            vehiculo = Vehiculo()
-            
+    def construirVehiculo(self,tipoVehiculo):
         if tipoVehiculo == 'Ford':
-            vehiculo.setMarca('Ford')
+            vehiculo = Ford()
         elif tipoVehiculo == 'Hyundai':
-            vehiculo.setMarca('Hyundai')
+            vehiculo = Hyundai()
         elif tipoVehiculo == 'Mercedes':
-            vehiculo.setMarca('Mercedes')
+            vehiculo = Mercedes()
         motor = Motor()
         vehiculo.setMotor(motor)
-        #vehiculo.instalacionPiezas()
-        #vehiculo.pintura()
-        #vehiculo.chasisCarroceria()
-        return vehiculo;
+        return vehiculo
+
 
 vF = VehiculoFactory()
 
-v1 = vF.construirVehiculo("Personas",'Ford')
-v2 = vF.construirVehiculo("Personas",'Hyundai')
-v3 = vF.construirVehiculo("Carga",'Mercedes')
+v1 = vF.construirVehiculo('Ford')
+v2 = vF.construirVehiculo('Hyundai')
+v3 = vF.construirVehiculo('Mercedes')
+
 
 print(v1.aceleracion(5))
-print(v1.transporta())
-print(v1.getMarca())
+print(v1.marca())
 print(v2.aceleracion(7))
-print(v2.transporta())
-print(v2.getMarca())
+print(v2.marca())
 print(v3.aceleracion(10))
-print(v3.transporta())
-print(v3.getMarca())
+print(v3.marca())
+
+print(v1.descipcion())
+v1.pintar('rojo')
+v1.agregarPuertas(4)
+v1.establecerCapacidad(6)
+v1.elegirPlaca('GOD333')
+print(v1.descipcion())
